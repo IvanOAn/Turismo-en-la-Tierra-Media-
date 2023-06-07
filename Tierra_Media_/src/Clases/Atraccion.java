3406dcc2eecc54657195f30a368f25c606ed7b81
Luis Di Nicco <102923308+LuisDiNicco@users.noreply.github.com>
2023-06-07 17:35:20 -0300

    Agrego test y cambio en Salida Archivos

--- Tierra_Media_/src/Clases/Atraccion.java
+++ Tierra_Media_/src/Clases/Atraccion.java
@@ -1,71 +1,37 @@
 package Clases;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Objects;
-import java.util.Set;
-import java.util.TreeSet;
 
-public class Atraccion implements Recomendacion {
+public class Atraccion extends Recomendacion{
 
-	private String nombre;
-	private double costo;
-	private double tiempo;
+	//private String nombre;
+	//private double costo;
+	//private double tiempo;
 	private int cupo;
-	private TipoDeAtraccion tipoDeAtraccion;
+	//private TipoDeAtraccion tipoDeAtraccion;
 
 	// -- Constructor --
-	public Atraccion(String nombre, double costo, double tiempo, int cupo, TipoDeAtraccion tipoDeAtraccion) {
-		this.nombre = nombre;
-		this.costo = costo;
-		this.tiempo = tiempo;
+	public Atraccion(String nombre, double precio, double duracion, int cupo, TipoDeAtraccion tipoDeAtraccion) {
+		super(nombre,precio,duracion,tipoDeAtraccion);
 		this.cupo = cupo;
-		this.tipoDeAtraccion = tipoDeAtraccion;
 	}
 
 	// -- Getters --
-	public double getCosto() {
-		return costo;
-	}
-
-	public double getTiempo() {
-		return tiempo;
-	}
-
-	public TipoDeAtraccion getTipoDeAtraccion() {
-		return tipoDeAtraccion;
-	}
 
 	public int getCupo() {
 		return cupo;
-	}
-
-	public String getNombre() {
-		return nombre;
 	}
 	
 	// -- Overrides --
 
 	@Override
 	public String toString() {
-		return "\nAtraccion: " + this.nombre + "\n" + "-Precio: $" + costo + "\n" + "-Tiempo Requerido: " + tiempo + "\n"
+		return "\nAtraccion: " + this.nombre + "\n" + "-Precio: $" + precio + "\n" + "-Duración: " + duracion + "\n"
 				+ "-Tipo de atraccion: " + tipoDeAtraccion;
 	}
 
-	@Override
-	public double getPrecio() {
-		return this.costo;
-	}
-
-	@Override
-	public TipoDeAtraccion getTipoDeRecomendacion() {
-		return this.tipoDeAtraccion;
-	}
-
-	@Override
+	/*@Override
 	public boolean recomendacionValida(Usuario usuario) {
 		HashSet<String> itinerario = usuario.getItinerario();
 		if (itinerario.contains(this.nombre))
@@ -76,45 +42,44 @@ public class Atraccion implements Recomendacion {
 	@Override
 	public void agregarRecomendacionAItinerario(Usuario usuario) {
 		usuario.agregarRecomendacion(this.nombre);
-	}
+	}*/
 
+/*	@Override
+	public void actualizarRecomendaciones(List<Atraccion> listaAtracciones,List<Promocion> listaPromociones){
+		Iterator<Atraccion> iterador=listaAtracciones.iterator();
+		
+		while(iterador.hasNext()) {
+			Atraccion atraccion=iterador.next();
+			if(atraccion.getNombre().equals(this.nombre)) {
+				if(atraccion.getCupo()==0)
+					iterador.remove();			}
+		}
+		
+		
+	}*/
+	
 	@Override
-	public void actualizarRecomendaciones(List<Atraccion> listaAtracciones, List<Promocion> listaPromociones,
-			HashMap<String, Atraccion> mapaAtracciones) {
-
-		for (Atraccion atraccion : listaAtracciones)
-			if (atraccion.getNombre().equals(this.nombre))
-				atraccion.decrementarCupo();
-
-		for (Promocion promocion : listaPromociones)
-			for (String nombreAtraccion : promocion.getAtraccionesIncluidas())
-				if (nombreAtraccion.equals(this.nombre))
-					promocion.recalcularCupo(listaAtracciones);
-	}
-
 	public void decrementarCupo() {
 		this.cupo -= 1;
 	}
 
 	@Override
-	public int prioridad() {
-		return 1;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(cupo);
+	public boolean esPromocion() {
+		return false;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null)
+		if (!super.equals(obj))
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
 		Atraccion other = (Atraccion) obj;
 		return cupo == other.cupo;
 	}
+
+
+	
 }
